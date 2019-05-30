e898de9757a5fdb601cc75633dbece8e6d3a23ed
mohamed.labouardy@gmail.com <mohamed@labouardy.com>
2019-05-30 14:21:31 +0200

    komiser:2.2.0

--- Formula/komiser.rb
+++ Formula/komiser.rb
@@ -1,9 +1,9 @@
 class Komiser < Formula
     desc "Komiser.io command line interface"
     homepage "https://komiser.io"
-    url "https://cli.komiser.io/2.1.0/osx/komiser"
-    version "2.1.0"
-    sha256 "69a47b840778d65b62ba3bd09ced7e10ba290ee05139690dbfb64b3673154163"
+    url "https://cli.komiser.io/2.2.0/osx/komiser"
+    version "2.2.0"
+    sha256 "08f557e824e465ca307e0609c68ee4d7d24d95aa1d986a132dc17f0019f81757"
   
     def install
       bin.install "komiser"
