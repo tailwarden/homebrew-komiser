3a4a8932d602e42702ca28d83bb714e8ef940588
LABOUARDY Mohamed <mohamed@labouardy.com>
2019-06-11 17:23:28 +0200

    fix sha256

--- Formula/komiser.rb
+++ Formula/komiser.rb
@@ -3,7 +3,7 @@ class Komiser < Formula
     homepage "https://komiser.io"
     url "https://cli.komiser.io/2.2.0/osx/komiser"
     version "2.2.0"
-    sha256 "08f557e824e465ca307e0609c68ee4d7d24d95aa1d986a132dc17f0019f81757"
+    sha256 "c9a2efb2dc0ef9ce10e2e80ae6ea5bc364ad73b70d3f6f46bbc1c7c4a16cfd27"
   
     def install
       bin.install "komiser"
