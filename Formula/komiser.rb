f0e57d7194a71987ddc6168b24fec5db47658388
Jake Page <jakepage@Jakes-MBP.Home>
2022-08-18 12:19:32 +0100

    updated to correst checksum

--- Formula/komiser.rb
+++ Formula/komiser.rb
@@ -3,7 +3,7 @@ class Komiser < Formula
     homepage "https://komiser.io"
     url "https://cli.komiser.io/2.9.0/osx/komiser"
     version "2.9.0"
-    sha256 "4f4bb16fb4673716de2643e916b1d60d3d1be09e9bdedeffa3d83fee9533a7b0"
+    sha256 "6b09fdffa41b8cddcca8ea5c91cf6a7570724c45e423315fcd604ef35a469d70"
   
     def install
       bin.install "komiser"
