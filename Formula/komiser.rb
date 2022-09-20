3281bdafad1e62a7174ca0a8cacb68cd5ae30bda
Jake Page <38757612+jakepage91@users.noreply.github.com>
2022-09-20 15:55:41 +0100

    update komiser version to 2.11.0

--- Formula/komiser.rb
+++ Formula/komiser.rb
@@ -1,8 +1,8 @@
 class Komiser < Formula
     desc "Komiser.io command line interface"
     homepage "https://komiser.io"
-    url "https://cli.komiser.io/2.10.0/osx/komiser"
-    version "2.10.0"
+    url "https://cli.komiser.io/2.11.0/osx/komiser"
+    version "2.11.0"
     sha256 "ecf0cfe06b53c535469d59b9acfd96f9c113bf5920cf7f984266db7c8780e6da"
   
     def install
