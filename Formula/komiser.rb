fb5b4568b20374695316c66ce387633a35d483a9
Jake Page <38757612+jakepage91@users.noreply.github.com>
2022-10-14 09:15:24 +0100

    revert sha256 to Komiser v2.11.0

--- Formula/komiser.rb
+++ Formula/komiser.rb
@@ -1,9 +1,9 @@
 class Komiser < Formula
     desc "Komiser.io command line interface"
     homepage "https://komiser.io"
-    url "https://cli.komiser.io/3.0.0/osx/komiser"
-    version "3.0.0"
-    sha256 "a956a54da6f2d880de5eb5f6f67cb40c216a5728a19b1e621be8fc3dd8076116"
+    url "https://cli.komiser.io/2.11.0/osx/komiser"
+    version "2.11.0"
+    sha256 "e96d79b6ae2170fab81c2cb71bc3cda4098bdc06bdc1f02e6be87fbcfb28d0e4"
   
     def install
       bin.install "komiser"
