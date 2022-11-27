2f81bbbe9e966b55e8220ae3851ba06fcb6b08c3
Mohamed Labouardy <mohamed@labouardy.com>
2022-11-27 12:54:50 +0100

    feat: support both arm and amd architectures

--- Formula/komiser.rb
+++ Formula/komiser.rb
@@ -1,11 +1,24 @@
 class Komiser < Formula
     desc "Komiser.io command line interface"
     homepage "https://komiser.io"
-    url "https://cli.komiser.io/2.11.0/osx/komiser"
-    version "2.11.0"
-    sha256 "e96d79b6ae2170fab81c2cb71bc3cda4098bdc06bdc1f02e6be87fbcfb28d0e4"
+    version "3.0.0"
+
+    on_macos do
+      if Hardware::CPU.intel?
+        url "https://cli.komiser.io/3.0.0/osx/amd/komiser"
+        sha256 "c048156fbfe2f2e29c49d03bbc4802d8a862ecb57875a118051ef7279e698e86"
   
-    def install
-      bin.install "komiser"
+        def install
+          bin.install "komiser"
+        end
+      end
+      if Hardware::CPU.arm?
+        url "https://cli.komiser.io/3.0.0/osx/arm/komiser"
+        sha256 "4025acf29a705068cc210cd54fc7c6d8b7792781fcf911085c90335352cc49a7"
+  
+        def install
+          bin.install "komiser"
+        end
+      end
     end
 end
