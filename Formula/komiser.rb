9ae4973b3fdb02af39b4073404e613c9a89db5c4
Jake Page <38757612+jakepage91@users.noreply.github.com>
2023-02-20 10:21:57 +0000

    Update komiser.rb

--- Formula/komiser.rb
+++ Formula/komiser.rb
@@ -14,7 +14,7 @@ class Komiser < Formula
       end
       if Hardware::CPU.arm?
         url "https://cli.komiser.io/3.0.3/osx/arm/komiser"
-        sha256 "1665ee0d22e25ff00c100398b3308d3d1821eec17899f14b4740cab5e01421c6"
+        sha256 "d66ace161dea6ab905034504ff800a6c6a39c772bf0a8847a34e271c2bd8a121"
   
         def install
           bin.install "komiser"
