c27941e76cba3f7d767342017ce3e74eab8e2df8
Jake Page <38757612+jakepage91@users.noreply.github.com>
2023-03-22 10:26:04 +0000

    Update komiser.rb minor update to 3.0.7

--- Formula/komiser.rb
+++ Formula/komiser.rb
@@ -1,20 +1,20 @@
 class Komiser < Formula
     desc "Komiser.io command line interface"
     homepage "https://komiser.io"
-    version "3.0.6"
+    version "3.0.7"
 
     on_macos do
       if Hardware::CPU.intel?
-        url "https://cli.komiser.io/3.0.6/osx/amd/komiser"
-        sha256 "cf5b3f89a7b2da629f96b9cc4a59ca3df3d8f9c4b119970a99996f34efaef826"
+        url "https://cli.komiser.io/3.0.7/osx/amd/komiser"
+        sha256 "12c8d39a91b680d91d904e6670368126055b7004c02c8ff37518e9e9326e6ab0"
   
         def install
           bin.install "komiser"
         end
       end
       if Hardware::CPU.arm?
-        url "https://cli.komiser.io/3.0.6/osx/arm/komiser"
-        sha256 "2bd3f25c564f2475c3c1a45413b4bcd2c952d87345b1e065300b8f24aee8a54e"
+        url "https://cli.komiser.io/3.0.7/osx/arm/komiser"
+        sha256 "e8d87fb2bfa299161ef35308982f1e59092a9e6acad037eef972c346d4a5c66e"
   
         def install
           bin.install "komiser"
